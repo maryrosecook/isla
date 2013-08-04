fce125554118c337a63ef4bca950da5c4c4fa0b6
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-08-04 18:37:43 -0400

    Remove merge.

--- src/utils.js
+++ src/utils.js
@@ -9,17 +9,4 @@
   exports.Utils.type = function(x) {
     return Object.prototype.toString.call(x).match(/\[object ([^\]]+)\]/)[1];
   };
-
-  exports.Utils.merge = function(a, b) {
-    var c = {};
-    for (var i in a) {
-      c[i] = a[i];
-    }
-
-    for (var i in b) {
-      c[i] = b[i];
-    }
-
-    return c;
-  }
 })(typeof exports === 'undefined' ? this.Isla : exports);
